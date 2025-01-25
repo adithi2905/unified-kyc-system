0e49386e0417c82160925ac542d50d7bff8fa0a0
adithi2905 <101107241+adithi2905@users.noreply.github.com>
2025-01-25 06:28:30 -0500

    Created fronend components structure

---
+++ frontend/my-app/src/components/auth/login.js
@@ -0,0 +1,18 @@
+import React ,{useState} from React;
+
+const Login=()=>{
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+
+    const handleLogin = async() =>{
+
+    };
+
+    return (
+        <div classname='login-container'>
+            <h2>Login</h2>
+            <input type="email" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+    </div>
+    )
+
+}
