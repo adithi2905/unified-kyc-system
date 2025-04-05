5f0e91681833bbe4a1d68c892477c9bbec722233
adithi2905 <101107241+adithi2905@users.noreply.github.com>
2025-04-05 16:48:53 -0400

    Added changes for the ssn extraction

---
+++ backend/src/main/java/com/kyc/service/SsnExtractionService.java
@@ -0,0 +1,42 @@
+package com.kyc.service;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kyc.entities.KycDetails;
+
+
+@Service
+public class SsnExtractionService {
+
+    KycDetails kycDetails=new KycDetails();
+    public String loadFile() throws IOException, InterruptedException{
+        ProcessBuilder pb = new ProcessBuilder("python", "ocr/ssnextractor.py", "resized.png");
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line="";
+        String ssnNo="";
+        while((line=reader.readLine())!=null)
+        {
+            
+        ssnNo=line;
+        }
+        process.waitFor();
+        kycDetails.setSsnNo(ssnNo);
+        if(!(ssnNo!=null && (ssnNo.equals(""))))
+            return "SSN is extracted successfully";
+        return "SSN is not found";
+    }
+
+    
+}
